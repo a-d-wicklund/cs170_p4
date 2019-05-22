0fba4d9c9287b2fad43fdee8a3179df5f30a60f2
adwcsil <awicklund@ucsb.edu>
2019-05-22 11:04:06 -0700

    Add some skeleton code

--- tls.c
+++ tls.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAXTHREADS
 
-int main(){
+struct tmem{
+    void *mem;
+}
 
+//Array to hold references to all threads with TLSs
+struct tmem[MAXTHREADS] = NULL;
 
+int tls_create(unsigned int size){
+    //Use mmap to create a new block of memory for the thread
 
 }
